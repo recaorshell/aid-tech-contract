173b6c2f40c8a737528dfc35001aca63b4d5d199
recaorshell <recaorshell@gmail.com>
2023-03-13 08:20:29 -0500

    fix: Remove the unnecessary comments

--- scripts/default-AID-testnet-deploy.ts
+++ scripts/default-AID-testnet-deploy.ts
@@ -9,21 +9,12 @@ async function main() {
         fs.mkdirSync(dir);
     }
 
-    // Obtain reference to contract and ABI.
     const DefaultAIDContract = await ethers.getContractFactory("DefaultAID");
     console.log("DefaultAID Contract is deploying to ", network.name);
 
-    //  Deploy logic contract using the proxy pattern.
     const DefaultAIDContract_ = await upgrades.deployProxy(
         DefaultAIDContract,
-
-        //Since the logic contract has an initialize() function
-        // we need to pass in the arguments to the initialize()
-        // function here.
         [50000000],
-
-        // We don't need to expressly specify this
-        // as the Hardhat runtime will default to the name 'initialize'
         {initializer: "initialize"}
     );
     await DefaultAIDContract_.deployed();
